5a3c7596f87387c51197a5183cff922a1913302e
AbleCats <etsy@live.com>
2023-04-04 14:13:48 +0800

    修改初始页面

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup lang="ts">
-import { useDark, useToggle } from '@vueuse/core'
-const isDark = useDark()
-const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <div>
-    <el-button @click="toggleDark">dark</el-button>
-  </div>
+  <RouterView/>
 </template>
 
 <style scoped>
